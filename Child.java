a88b2cc408d033c11467a36f006ba5600dffa6b3
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-03-09 18:48:32 +0900

    final method 예제

---
+++ Child.java
@@ -0,0 +1,19 @@
+package final_.method;
+
+public class Child extends Parent {
+
+	// 오버라이딩 (alt + shift + s)
+
+	@Override // 어노테이션 오버라이딩이 됬다는 의미
+	public void method01() {
+
+		
+	}
+	
+//	public final void method02() {
+//		
+//	}
+
+
+
+}
---
+++ Parent.java
@@ -0,0 +1,11 @@
+package final_.method;
+
+public /*final*/ class Parent { 
+	
+	
+	// fianl 키워드가 클래스에 붙으면 상속 금지, 메서드에 붙으면 오버라이딩 금지
+	public void method01() {}
+	
+	public final void method02() {}
+
+}
